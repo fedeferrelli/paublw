ee9aba920289e86fa08942ff75aa51a901a6acb2
Fede <ferrelli.mazza@gmail.com>
2022-06-16 19:45:31 -0300

    Restyle comments page

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -141,7 +141,7 @@ function HeroSection() {
     
           {/* Imagen */}
     
-          <div className="lg:absolute -z-10 lg:inset-y-0 lg:right-0 lg:w-1/2">
+          <div className="lg:absolute -z-10 lg:inset-y-0 lg:right-0 lg:w-1/2 hidden sm:flex">
             <img
               className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
               src={Image}
--- src/Components/Opiniones.jsx
+++ src/Components/Opiniones.jsx
@@ -9,11 +9,11 @@ function Opiniones() {
         <Fade fraction="0" duration="1000" triggerOnce>
         
            
-            <div className="m-auto sm:h-auto sm:w-full flex flex-wrap sm:flex-row justify-center gap-4">
+            <div className="m-auto sm:h-auto sm:w-full flex flex-wrap sm:flex-row justify-center gap-4 ">
    {opiniones.map(opinion=>(
-               <div key={opinion.name} className="w-full  p-8 rounded-lg  sm:w-1/3 sm:w-max-[450px] shadow-gray-300 shadow-xl flex flex-col border-0 border-secondary"> 
-               <h1 className="m-auto text-secondary font-bold">{opinion.names}</h1>
-               <p className="tracking-wider max-w-prose text-base text-gray-600 sm:text-lg md:text-xl text-center font-light">{opinion.opinion}</p> 
+               <div key={opinion.name} className="w-full  py-6 px-2 rounded-lg  sm:w-1/3 sm:w-max-[450px]   shadow-lg shadow-terciary border border-terciary/20 flex flex-col "> 
+               <h1 className="m-auto text-secondary font-bold sm:text-lg md:text-xl">{opinion.names}</h1>
+               <p className="mt-2 tracking-wider max-w-prose text-base text-gray-600 sm:text-lg md:text-xl text-center font-light">{opinion.opinion}</p> 
                
                
                </div>
